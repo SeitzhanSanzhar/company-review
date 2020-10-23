80d91b777c27200dda900fbf001faefeda0a4f65
Adilet Absatov <adl.absatov@gmail.com>
2020-10-23 21:49:55 +0600

    Change button colors

--- src/components/reviews/review-item/ReviewItem.tsx
+++ src/components/reviews/review-item/ReviewItem.tsx
@@ -1,8 +1,9 @@
-import React, {ReactElement, useEffect, useState} from "react";
+import React, { ReactElement, useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import { Link } from "react-router-dom";
 import { Review } from "../../../models/Review";
-import {Link} from "react-router-dom";
+import './ReviewItem.css';
 
 type ReviewProps = {
     review: Review;
@@ -26,10 +27,10 @@ export default function ReviewItem({review, changeReviewLikes, isView}: ReviewPr
                 </Card.Text>
                 <p className="text-success">Likes: {review.likes}</p>
                 {!isView &&
-                <Link to={`/reviews/${review.id}`}><Button variant="primary m-2">View Post</Button></Link>
+                <Link to={`/reviews/${review.id}`}><Button className="view-post" variant="primary m-2">View Post</Button></Link>
                 }
-                <Button variant="success m-2" onClick={() => setlikes(likes + 1)}>Like</Button>
-                <Button variant="danger m-2" onClick={() => setlikes(likes - 1)}>Dislike</Button>
+                <Button className="like" variant="success m-2" onClick={() => setlikes(likes + 1)}>Like</Button>
+                <Button className="dislike" variant="danger m-2" onClick={() => setlikes(likes - 1)}>Dislike</Button>
                 {isView &&
                 <Link to={`/reviews`}><Button variant="primary m-2">Back</Button></Link>
                 }
