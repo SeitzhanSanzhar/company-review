20c8ca1eafddf579dab6981aa6b6dbb4c9e93a85
kozhakhmetov <adilkhan.kozhakhmetov@gmail.com>
2020-10-23 19:04:39 +0600

    ref added

--- src/components/reviews/review-add/ReviewAdd.tsx
+++ src/components/reviews/review-add/ReviewAdd.tsx
@@ -9,27 +9,36 @@ type Props = {
 };
 
 export default function ReviewAdd({addReview}: Props): ReactElement {
+
+    const authorref = React.useRef<HTMLInputElement>(null);
     const companyref = React.useRef<HTMLInputElement>(null);
     const reviewref = React.useRef<HTMLTextAreaElement>(null);
 
     function handleSubmit(event: FormEvent<HTMLElement>):void {
         console.log('qwerqwerqwerqe');
-        if (companyref.current && reviewref.current && companyref.current.value && reviewref.current.value) {
+        if (companyref.current && reviewref.current &&
+            companyref.current.value && reviewref.current.value &&
+            authorref.current && authorref.current.value) {
             const review: Review = {id:0,companyName: companyref.current.value, review: reviewref.current.value, likes: 0, author: 'todo'};
             companyref.current.value = '';
             reviewref.current.value = '';
+            authorref.current.value = '';
             addReview(review);
         }
     }
     return(
         // eslint-disable-next-line react/jsx-no-undef
         <Form>
-            <Form.Group controlId="exampleForm.ControlInput1">
+            <Form.Group>
+            <Form.Label>Author</Form.Label>
+            <Form.Control type="text" placeholder="Author" ref={authorref}/>
+        </Form.Group>
+            <Form.Group>
                 <Form.Label>Company</Form.Label>
                 <Form.Control type="text" placeholder="Company" ref={companyref}/>
             </Form.Group>
 
-            <Form.Group controlId="exampleForm.ControlTextarea1">
+            <Form.Group>
                 <Form.Label>Review</Form.Label>
                 <Form.Control as="textarea" rows={10} placeholder="Review" ref={reviewref}/>
             </Form.Group>
