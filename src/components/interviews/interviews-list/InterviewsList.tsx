d653db450f595666ecb4139c9a0afeefa0e7cc51
Adilet Absatov <adl.absatov@gmail.com>
2020-10-23 17:42:58 +0600

    Add interview

--- src/components/interviews/interviews-list/InterviewsList.tsx
+++ src/components/interviews/interviews-list/InterviewsList.tsx
@@ -4,50 +4,24 @@ import InterviewItem from '../interview-item/InterviewItem';
 import './InterviewsList.css';
 
 interface Props {
-    
+    interviews: Interview[];
 }
 interface State {
-    
+    interviews: Interview[];
 }
 
-const interviews: Interview[] = [
-    {
-        id: 1,
-        topic: "Road to Google",
-        author: "Adilet",
-        text: "Contest was quite difficult and I enjoyed host matching interviews. Unfortunately, process was postponed due to COVID",
-        verdict: "Postponed"
-    },
-    {
-        id: 2,
-        topic: "Phone call with Jane Street",
-        author: "Jose",
-        text: "There was only one problem with follow ups, you have to perform perfect to advance futher",
-        verdict: "Reject"
-    },
-    {
-        id: 1,
-        topic: "Facebook phone interview",
-        author: "Adilet",
-        text: "I had two phone interviews at Facebook London for an internship. There were total of 4 problems and a behavioral interview. Overall this was a great experience",
-        verdict: "Offer"
-    },
-    {
-        id: 4,
-        topic: "OneTechnology onsite",
-        author: "Peter",
-        text: "This was a strange interview, I did not feel like I achieved something after I got an offer",
-        verdict: "Offer"
-    },
-];
-
 export default class InterviewsList extends Component<Props, State> {
-    state = {}
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            interviews: this.props.interviews,
+        }
+    }
 
     render() {
         return (
             <div className="interview-container">
-                {interviews.map((interview) => <InterviewItem interview={interview}/>)}
+                {this.state.interviews.map((interview) => <InterviewItem interview={interview}/>)}
             </div>
         )
     }
--- src/components/interviews/interviews-page/InterviewsPage.tsx
+++ src/components/interviews/interviews-page/InterviewsPage.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Interview } from '../../../models/Interview';
 import InterviewAdd from '../interview-add/InterviewAdd';
 import InterviewsList from '../interviews-list/InterviewsList';
 import './InterviewsPage.css';
@@ -9,6 +10,37 @@ interface Props {
 interface State {
     showDiscover: boolean;
 }
+
+const interviews: Interview[] = [
+    {
+        id: 1,
+        topic: "Road to Google",
+        author: "Adilet",
+        text: "Contest was quite difficult and I enjoyed host matching interviews. Unfortunately, process was postponed due to COVID",
+        verdict: "Postponed"
+    },
+    {
+        id: 2,
+        topic: "Phone call with Jane Street",
+        author: "Jose",
+        text: "There was only one problem with follow ups, you have to perform perfect to advance futher",
+        verdict: "Reject"
+    },
+    {
+        id: 1,
+        topic: "Facebook phone interview",
+        author: "Adilet",
+        text: "I had two phone interviews at Facebook London for an internship. There were total of 4 problems and a behavioral interview. Overall this was a great experience",
+        verdict: "Offer"
+    },
+    {
+        id: 4,
+        topic: "OneTechnology onsite",
+        author: "Peter",
+        text: "This was a strange interview, I did not feel like I achieved something after I got an offer",
+        verdict: "Offer"
+    },
+];
 
 export default class InterviewsNavbar extends Component<Props, State> {
     discoverButtonRef: React.RefObject<HTMLAnchorElement>;
@@ -46,8 +78,10 @@ export default class InterviewsNavbar extends Component<Props, State> {
         this.setState({showDiscover: false});
     }
 
-    addInterview() {
-
+    addInterview(interview: Interview) {
+        console.log(interview);
+        interview.id = interviews.length + 1;
+        interviews.push(interview);
     }
 
     render() {
@@ -70,7 +104,7 @@ export default class InterviewsNavbar extends Component<Props, State> {
                 </div>
                 </nav>
 
-                {this.state.showDiscover ? <InterviewsList/> : <InterviewAdd addInterview={this.addInterview}/>}
+                {this.state.showDiscover ? <InterviewsList interviews={interviews}/> : <InterviewAdd addInterview={this.addInterview}/>}
             </div>
         );
     }
